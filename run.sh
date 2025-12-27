39471e00847ba2b793a27a9a5502392e10223b0e
身無彩鳳雙飛翼──陸小雞 <32191137+pas-by@users.noreply.github.com>
2025-12-27 15:01:39 +0800

    initial vesion

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#  comply xmrig
+if [ ! -d xmrig/build ]; then
+    mkdir xmrig/build && cd xmrig/build
+    cmake ..
+    make -j$(nproc)
+    cd ..
+    cd ..
+fi
+
+#  run xmrig
+./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 8BghYaVYHXfW5GWzG1o1oF9Lk1mwHm9YhjoKMxeJrUoj4SPrrrGp6NsRc6FVbwHuSwX49odmezQRN3fh7NWHk7D7L1VA4Qv --randomx-1gb-pages --asm=ryzen -p ryzen5_8600G
