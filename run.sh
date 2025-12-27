e743282c1ea0794fa0f1614b52d2889317266404
身無彩鳳雙飛翼──陸小雞 <32191137+pas-by@users.noreply.github.com>
2025-12-27 15:25:41 +0800

    Change asm option from 'ryzen' to 'suto'

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ if [ ! -d xmrig/build ]; then
 fi
 
 #  run xmrig
-./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 8BghYaVYHXfW5GWzG1o1oF9Lk1mwHm9YhjoKMxeJrUoj4SPrrrGp6NsRc6FVbwHuSwX49odmezQRN3fh7NWHk7D7L1VA4Qv --randomx-1gb-pages --asm=ryzen -p ryzen5_8600G
+./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 8BghYaVYHXfW5GWzG1o1oF9Lk1mwHm9YhjoKMxeJrUoj4SPrrrGp6NsRc6FVbwHuSwX49odmezQRN3fh7NWHk7D7L1VA4Qv --randomx-1gb-pages --asm=suto -p ryzen5_8600G
